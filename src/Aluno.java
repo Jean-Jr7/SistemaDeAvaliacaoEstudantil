5c50a56939d653df34956e5afc5b22160fc9a7c7
Jean-Jr7 <jean.jose102013@gmail.com>
2024-01-31 21:24:46 -0300

    implementacao da classe listDeAlunos

--- src/Aluno.java
+++ src/Aluno.java
@@ -8,18 +8,35 @@ class Aluno {
     private int av2;
     private int media;
     private int avFinal;
-    public Aluno(String nome, int av1, int av2) {
+
+    private Aluno(String nome, int av1, int av2) {
         this.nome = nome;
         this.av1 = av1;
         this.av2 = av2;
         this.media = (av1 + av2) / 2;
+
     }
-    protected void verificar3prova(){
+    public static Aluno criarAlunoComEntrada(Scanner scanner) {
+
+        System.out.println("Digite o nome do Aluno:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a nota da av1 do aluno:");
+        int av1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite a nota da av2 do aluno:");
+        int av2 = Integer.parseInt(scanner.nextLine());
+
+
+
+        return new Aluno(nome, av1, av2);
+    }
+
+     void verificar3prova(){
         if (!passouSemTerceiraProva()){
             System.out.println("Digite a nota da Av3");
              this.avFinal = Integer.parseInt(scanner.nextLine());
         }
-
     }
     public String getNome() {
         return nome;
@@ -33,5 +50,6 @@ class Aluno {
     protected boolean aprovadoComTerceiraProva() {
         return ((avFinal + media) / 2 >= 6);
     }
+
 
 }
---
+++ src/ListaDeAlunos.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListaDeAlunos {
+    private List<Aluno> aprovados;
+    private List<Aluno> reprovados;
+    private List<Aluno> aprovados3Prova;
+
+    public ListaDeAlunos() {
+        this.aprovados = new ArrayList<>();
+        this.reprovados = new ArrayList<>();
+        this.aprovados3Prova = new ArrayList<>();
+    }
+
+    public void classificarAluno(Aluno aluno) {
+        if (aluno.passouSemTerceiraProva()) {
+            aprovados.add(aluno);
+        } else if (aluno.aprovadoComTerceiraProva()) {
+            aprovados3Prova.add(aluno);
+        } else {
+            reprovados.add(aluno);
+        }
+    }
+
+    public void imprimirRelatoriosDeAlunos() {
+
+        System.out.println("Alunos Aprovados");
+        for (Aluno aluno : aprovados) {
+            System.out.println("Nome: " + aluno.getNome() + "\n" + "Media:" + aluno.getMedia());
+        }
+
+        System.out.println("\nAlunos Reprovados:");
+        for (Aluno aluno : reprovados) {
+            System.out.println("Nome: " + aluno.getNome() + "\n" + "Media:" + aluno.getMedia());
+        }
+
+        System.out.println("\nAlunos Aprovados apos a terceira prova:");
+        for (Aluno aluno : aprovados3Prova) {
+            System.out.println("Nome: " + aluno.getNome() + "\n" + "Media:" + aluno.getMedia());
+        }
+    }
+
+
+}
